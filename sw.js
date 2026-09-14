5533aa5bb8535f0f3ed816b238d81706394ea907
kitty-sin <cathy2136@gmail.com>
2026-09-13 21:52:34 -0700

    fix(search): add dedicated search button, debounce input and protect Korean IME composition

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-﻿// KITTY 韓語積木大冒險 - Service Worker 離線快取 v1.0.2
-const CACHE_NAME = 'kitty-korean-v1.0.2';
+// KITTY 韓語積木大冒險 - Service Worker 離線快取 v1.0.3
+const CACHE_NAME = 'kitty-korean-v1.0.3';
 const STATIC_ASSETS = [
   './',
   './index.html',
