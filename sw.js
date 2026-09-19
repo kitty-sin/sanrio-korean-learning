c75744e589e204035b5638710acb286d3577d48e
kitty-sin <cathy2136@gmail.com>
2026-09-18 23:13:30 -0700

    chore: rename pdf button to 發音教學PDF(20頁)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// KITTY 韓語積木大冒險 - Service Worker 離線快取 v1.0.29
-const CACHE_NAME = 'kitty-korean-v1.0.29';
+// KITTY 韓語積木大冒險 - Service Worker 離線快取 v1.0.30
+const CACHE_NAME = 'kitty-korean-v1.0.30';
 const STATIC_ASSETS = [
   './',
   './index.html',
