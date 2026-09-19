7887f1a336303eed2f9f646a33da057d759033b2
kitty-sin <cathy2136@gmail.com>
2026-09-18 22:39:59 -0700

    fix: elevate resources banner to page top and add prominent tenses links on index.html

--- www/sw.js
+++ www/sw.js
@@ -1,5 +1,5 @@
-// KITTY 韓語積木大冒險 - Service Worker 離線快取 v1.0.26
-const CACHE_NAME = 'kitty-korean-v1.0.26';
+// KITTY 韓語積木大冒險 - Service Worker 離線快取 v1.0.27
+const CACHE_NAME = 'kitty-korean-v1.0.27';
 const STATIC_ASSETS = [
   './',
   './index.html',
